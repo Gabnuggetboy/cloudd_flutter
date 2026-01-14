f0b0a87d4c5b52c1b1be3857e991c3a300c66c91
PixelatedGoogle <adityapenkar@gmail.com>
2026-01-14 17:55:34 +0800

    Added more variables
    Allows User model to store 3 new variables, name, date of birth and profile pictures. Also added a save function to be allow the user to save their details

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,24 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 
 class AppUser {
   final String uid;
   final String email;
   final String role;
+  final String name;
+  final Timestamp? dateOfBirth;
+  final String? profileImageUrl;
   final Timestamp? createdAt;
 
   AppUser({
     required this.uid,
     required this.email,
     this.role = 'User',
+    this.name = '',
+    this.dateOfBirth,
+    this.profileImageUrl,
+
     this.createdAt,
   });
 
   factory AppUser.fromDoc(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>? ?? {};
+
     return AppUser(
       uid: doc.id,
       email: data['email'] as String? ?? '',
       role: data['role'] as String? ?? 'User',
+      name: data['name'] as String? ?? '',
+      dateOfBirth: data['date_of_birth'] as Timestamp?,
+      profileImageUrl: data['profile_image_url'] as String?,
       createdAt: data['created_at'] as Timestamp?,
     );
   }
@@ -27,10 +39,27 @@ class AppUser {
     return {
       'email': email,
       'role': role,
+      'name': name, 
+      'date_of_birth': dateOfBirth,
+      'profile_image_url': profileImageUrl,
       'created_at': createdAt ?? FieldValue.serverTimestamp(),
     };
   }
 
+  Future<void> save() async {
+  try {
+    await FirebaseFirestore.instance
+        .collection('users')
+        .doc(uid)
+        .set(toMap());
+    debugPrint("Firestore user created: $uid");
+  } catch (e) {
+    debugPrint("Firestore write failed: $e");
+    rethrow;
+  }
+}
+
+    
   bool get isManager => role == 'Manager';
   bool get isUser => role == 'User';
 }
